b2ad0e8e49b141ed29e460e2ebf5162f071c1329
Unknown <gerjon2@gmail.com>
2017-11-17 15:31:19 +0100

    oeps
    had nog save left

--- Brightness/BrightnessT1/BrightnessT1/BrightnessT1.ino
+++ Brightness/BrightnessT1/BrightnessT1/BrightnessT1.ino
@@ -16,14 +16,15 @@ ISR(TIMER1_OVF_vect){
 }
 
 int main(void){ //arduino start
-	init();
-	Serial.begin(BAUDRATE);
-	DDRC |= (1 << PORTC5);	/* Set all pins as input */
-	int brichtnes;
-	TCCR2A |= (1 << CS02) | (1 << CS00);
-	TIMSK1 |= (1<<TOIE0);
-	TCNT1 = 0;
-	sei();						//	enable interrupts!
+	DDRB |= (1<<DDB3);
+	Serial.begin(9600);
+	while(1)
+	{
+		single_sample();
+		OCR2A = step;
+		Serial.println(step);
+		_delay_ms(10);
+	}
 	
 	//	MI0283QT9 lcd;  //MI0283QT9 Adapter v1
 	uint8_t clear_bg=0x00; //0x80 = dont clear background for fonts (only for DisplayXXX)
