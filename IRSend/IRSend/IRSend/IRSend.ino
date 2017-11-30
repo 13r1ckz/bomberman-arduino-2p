f2d9de3cecabcafbfeda004391a42534543000f5
Unknown <s1077486@student.windesheim.nl>
2017-11-30 11:40:39 +0100

    Byte verzenden
    Er kan een byte verzonden worden met de functie sendByte

---
+++ IRSend/IRSend/IRSend/IRSend.ino
@@ -0,0 +1,129 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+int teller, currentTime;
+char pwmOn;
+
+void setPWM38();
+void setPWM56();
+void setInterrupt();
+void sendByte();
+void sendBit();
+void sendStartBit();
+void sendStopBit();
+void sendPulse();
+
+
+ISR(TIMER2_COMPB_vect){
+	
+}
+
+ISR(TIMER1_OVF_vect) {		//macro met interrupt vector
+	teller++;
+	//PORTD ^= (1<<PORTD6);		//...toggle LED D6
+}
+
+int main(void){
+
+	Serial.begin(9600);
+	
+	setPWM38();
+	setInterrupt();
+	//setPWM56();
+	
+	while(1){
+		sendByte('c');
+		Serial.println(" ");
+		_delay_ms(1000);
+	}
+	//while(1);
+	
+}
+
+void sendByte(uint8_t command){
+	uint8_t temporaryCommand = command;
+	sendStartBit();
+	
+	while(temporaryCommand){
+		Serial.println(temporaryCommand, BIN);
+		
+		if(temporaryCommand & (1<<7)){
+			sendBit(1);
+			} else {
+			sendBit(0);
+		}
+		temporaryCommand <<= 1;
+	}
+	sendStopBit();
+}
+
+void sendStartBit(){
+	sendPulse();
+
+	teller = 0;
+	while(!(teller == 30)){
+		Serial.print("");
+	}
+}
+
+void sendStopBit(){
+	sendStartBit();
+	teller = 0;
+	sendPulse();
+}
+
+void sendBit(char b){
+	sendPulse();
+	
+	teller = 0;
+	
+	if(b == 1){
+		while(!(teller == 20)){
+			Serial.print("");
+		}
+		} else {
+		while(!(teller == 10)){
+			Serial.print("");
+		}
+	}
+}
+
+void sendPulse(){
+	teller = 0;
+	TCCR2A |= (1 << COM2B1);
+	while(!(teller == 10)){
+		Serial.print("");
+	}
+	TCCR2A &= ~(1 << COM2B1);
+}
+
+void setPWM38(){
+	TCCR2A =  (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+	TCCR2B = (1 << WGM22) | (1 << CS21); //Fast PWM & prescaler /8 - 16000000/8/52 / 38,4 KHz
+	OCR2A = 51;
+	OCR2B = 26;  // Duty Cycle 50%
+	TIMSK2 |= (1<<OCIE2B);
+
+	DDRD |= (1<<DDD3) ; //pin 3
+	pwmOn = 1;
+}
+
+void setPWM56(){
+	TCCR2A =  (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+	TCCR2B = (1 << WGM22) | (1 << CS21); //Fast PWM & prescaler /8 - 16000000/8/36 / 55,5 KHz
+	OCR2A = 35;
+	OCR2B = 17;  // Duty Cycle 50%
+	TIMSK2 |= (1<<OCIE2B);
+
+	DDRD |= (1<<DDD3) ; //pin 3
+	pwmOn = 1;
+}
+
+void setInterrupt(){
+	DDRD |= (1<<DDD6);		//setup (digital pen 6 = PD6)
+	TCCR1B |= (1 << CS11);// | (1<<CS20);
+	TIMSK1 |= (1<<TOIE1);
+	TCNT1 = 0;
+	sei();
+}
+
