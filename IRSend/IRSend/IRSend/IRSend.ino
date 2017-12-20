9bae2a07c3227ebc87988ed6d242b32893ce3924
Unknown <s1077486@student.windesheim.nl>
2017-12-20 13:51:51 +0100

    IRcom.h/cpp gemaakt
    Het verzenden en ontvangen naar h file en cpp file gezet.

--- IRSend/IRSend/IRSend/IRSend.ino
+++ IRSend/IRSend/IRSend/IRSend.ino
@@ -1,184 +1,69 @@
+#include <Arduino.h>
 #include <avr/interrupt.h>
-#include <avr/io.h>
+#include "lib/IRcom/IRcom.h"
 
-//zender
-int tellerZender;
-//ontvanger
-uint8_t ontvangenbericht= 0x00;
-int tellerontvanger = 0;
-int tempteller = 0;
-int startbit = 0;
-signed char bitteller = 7;
-char letter;
-int ontvangeraantal=0;
-int verschil;
-void setPWM38();
-void setPWM56();
-void setInterrupt();
-void sendByte();
-void sendBit();
-void sendStartBit();
-void sendStopBit();
-void sendPulse();
-void initInterrupt0();
-
+IRcom ir;
 
 ISR(TIMER2_COMPB_vect){
 }
 
 ISR(TIMER1_OVF_vect) {    //macro met interrupt vector
-	tellerZender++;
-	tellerontvanger++;
+	ir.setTellerVerzender(ir.getTellerVerzender() + 1);
+	ir.setTellerOntvanger(ir.getTellerOntvanger() + 1);
 }
 
 ISR(INT0_vect){
 
-	verschil = tellerontvanger - tempteller;
-	//Serial.println(verschil);
+	ir.verschil = ir.getTellerOntvanger() - ir.tempteller;
 	
-	if(startbit == 0){
-		if(verschil >= 40){
-			startbit = 1;
-			//Serial.println("Startbit");
-			ontvangeraantal++;
+	if(ir.startbit == 0){
+		if(ir.verschil >= 40){
+			ir.startbit = 1;
+			ir.ontvangeraantal++;
 		}
-	} else if (startbit == 1)
-	
-	{
-		
-		if(verschil >= 40){
-			startbit = 0;
-			//Serial.println("Stopbit");
-			tellerontvanger = 0;
-			tempteller = 0;
-			ontvangeraantal++;
-			} else if(verschil >= 30 && verschil <40){
-			//Serial.print("1");
-			ontvangenbericht |=(1<<bitteller);
-			bitteller--;
-			ontvangeraantal++;
-			
-			} else if(verschil >= 20 && verschil <30){
-			//Serial.print("0");
-			ontvangenbericht &=~(1<<bitteller);
-			bitteller--;
-			ontvangeraantal++;
-			
+		} else if (ir.startbit == 1){
+		if(ir.verschil >= 40){
+			ir.startbit = 0;
+			ir.setTellerOntvanger(0);
+			ir.tempteller = 0;
+			ir.ontvangeraantal++;
+			} else if(ir.verschil >= 30 && ir.verschil <40){
+			ir.ontvangenbericht |=(1<<ir.bitteller);
+			ir.bitteller--;
+			ir.ontvangeraantal++;
+			} else if(ir.verschil >= 20 && ir.verschil <30){
+			ir.ontvangenbericht &=~(1<<ir.bitteller);
+			ir.bitteller--;
+			ir.ontvangeraantal++;
 		}
 	}
 	
-	if(ontvangeraantal % 10 == 0){
-		if(bitteller == -1){
-			bitteller =7;
-			letter = ontvangenbericht;
-			Serial.print(letter);
-			ontvangenbericht = 0x00;
+	if(ir.ontvangeraantal % 10 == 0){
+		if(ir.bitteller == -1){
+			ir.bitteller = 7;
+			ir.letter = ir. ontvangenbericht;
+			Serial.println(ir.letter);
+			ir.ontvangenbericht = 0x00;
 			}else{
-			bitteller =7;
-			ontvangenbericht = 0x00;
+			ir.bitteller =7;
+			ir.ontvangenbericht = 0x00;
 		}
 	}
-	tempteller = tellerontvanger;
+	ir.tempteller = ir.getTellerOntvanger();
 }
+
 int main(void){
 	Serial.begin(9600);
-	//setPWM38();
-	setInterrupt();
 	
-	initInterrupt0();
-	setPWM56();
+	ir.setIR();
+	
 	while(1){
 		if(Serial.available()){
 			char letter = Serial.read();
-			sendByte(letter);
+			ir.sendByte(letter);
 		}
 	}
-}
-
-void sendByte(uint8_t command){
-	uint8_t temporaryCommand = command;
-	sendStartBit();
-	char i;
-	//Serial.println(temporaryCommand, BIN);
-	for(i = 0; i < 8; i++){
-		if(temporaryCommand & (1<<7)){
-			sendBit(1);
-			} else {
-			sendBit(0);
-		}
-		temporaryCommand <<= 1;
-	}
-	sendStopBit();
-	TCCR2A |= (1 << COM2B1);
-}
-
-void sendStartBit(){
-	sendPulse();
-	tellerZender = 0;
-	while(!(tellerZender == 30)){
-		Serial.print("");
-	}
-}
-
-void sendStopBit(){
-	sendStartBit();
-	tellerZender = 0;
-	sendPulse();
-}
-
-void sendBit(char b){
-	sendPulse();
-	tellerZender = 0;
-	if(b == 1){
-		while(!(tellerZender ==20)){
-			Serial.print("");
-		}
-		} else {
-		while(!(tellerZender == 10)){
-			Serial.print("");
-		}
-	}
-}
-
-void sendPulse(){
-	tellerZender = 0;
-	TCCR2A |= (1 << COM2B1);
-	while(!(tellerZender == 10)){
-		Serial.print("");
-	}
-	TCCR2A &= ~(1 << COM2B1);
-}
-
-void setPWM38(){
-	TCCR2A =  (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-	TCCR2B = (1 << WGM22) | (1 << CS21); //Fast PWM & prescaler /8 - 16000000/8/52 / 38,4 KHz
-	OCR2A = 51;
-	OCR2B = 26;  // Duty Cycle 50%
-	TIMSK2 |= (1<<OCIE2B);
-	DDRD |= (1<<DDD3) ; //pin 3
-}
-
-void setPWM56(){
-	TCCR2A =  (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-	TCCR2B = (1 << WGM22) | (1 << CS21); //Fast PWM & prescaler /8 - 16000000/8/36 / 55,5 KHz
-	OCR2A = 35;
-	OCR2B = 17;  // Duty Cycle 50%
-	TIMSK2 |= (1<<OCIE2B);
-	DDRD |= (1<<DDD3) ; //pin 3
-}
-
-void setInterrupt(){
-	TCCR1B |= (1 << CS10);
-	TIMSK1 |= (1<<TOIE1);
-	TCNT1 = 0;
-	sei();
-}
-
-void initInterrupt0()
-{
-	DDRD &=~(1<<DDD2);
-	PORTD |= (1<<PORTD2);
-	EIMSK |= (1 << INT0);
-	EICRA |= (1<<ISC01);
+	
+	return;
 	
 }
